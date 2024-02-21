c1ceb597923eba266a219e10171deb55e6c48bc6
JosephQ <100705345+JIQC15@users.noreply.github.com>
2024-02-21 13:37:30 -0600

    Push all the data
    Insert the files about the program

---
+++ src/Package/MainClass.java
@@ -0,0 +1,9 @@
+//Crea un programa con el famoso "Hola Mundo" que lo muestre en consola.
+
+package Package;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
